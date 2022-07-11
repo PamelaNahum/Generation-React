fa9dd66788e1e08426177221d03a103ac344f951
PamelaNahum <pamela.nahum14@gmail.com>
2022-07-11 15:53:04 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button } from "bootstrap";
+import React from "react";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-4">
+      <div className="row">
+        <div className="col-8 ">
+          <h1>Lista</h1>
+          
+        </div>
+        <div className="col">
+          <h1>Formulario</h1>
+          <form>
+            <div class="form-group">
+              <label>Nombre tarea</label>
+              <input
+                type="text"
+                class="form-control"
+                id="nombre_tarea"
+                placeholder="Nombre tarea"
+              />
+            </div>
+            <br/>
+            <div class="form-group">
+              <label>Descripcion</label>
+              <input
+                type="text"
+                class="form-control"
+                id="descripcion"
+                placeholder="Descripcion"
+              />
+            </div>
+            <br/>
+            <button type="submit" class="btn btn-outline-primary">
+              Crear
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
