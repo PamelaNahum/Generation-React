e77e0b16a5c74d0d478c73c9e8bbbd036f6df995
PamelaNahum <pamela.nahum14@gmail.com>
2022-07-11 15:58:15 -0400

    delete form

--- src/App.js
+++ src/App.js
@@ -11,31 +11,7 @@ const App = () => {
         </div>
         <div className="col">
           <h1>Formulario</h1>
-          <form>
-            <div class="form-group">
-              <label>Nombre tarea</label>
-              <input
-                type="text"
-                class="form-control"
-                id="nombre_tarea"
-                placeholder="Nombre tarea"
-              />
-            </div>
-            <br/>
-            <div class="form-group">
-              <label>Descripcion</label>
-              <input
-                type="text"
-                class="form-control"
-                id="descripcion"
-                placeholder="Descripcion"
-              />
-            </div>
-            <br/>
-            <button type="submit" class="btn btn-outline-primary">
-              Crear
-            </button>
-          </form>
+          
         </div>
       </div>
     </div>
