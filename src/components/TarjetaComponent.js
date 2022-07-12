48795253ea4a9617f120df314fb457c2e29b756f
PamelaNahum <pamela.nahum14@gmail.com>
2022-07-12 13:09:31 -0400

    componente dinamico

--- src/components/TarjetaComponent.js
+++ src/components/TarjetaComponent.js
@@ -1,18 +1,18 @@
 import React from "react";
 
-const TarjetaComponent = () => {
+const TarjetaComponent = ({usuario}) => {
     return(
     <div className = "card">
     <div className = "card-body">
-      <h3 className="card-title">Nombre Apellido</h3>
-      <p>Edad: 25</p>
+      <h3 className="card-title">{usuario.nombre} {usuario.apellido}</h3>
+      <p>Edad: {usuario.edad}</p>
       <hr/>
       <div className="d-flex justify-content-end">
         <button className="btn btn-sm btn-outline-primary me-2">Editar</button>
         <button className="btn btn-sm btn-outline-danger">Eliminar</button>
       </div>
     </div>
-  </div>);
+  </div>)
 }
 
 export default TarjetaComponent;
--- src/components/UsuarioComponent.js
+++ src/components/UsuarioComponent.js
@@ -2,6 +2,40 @@ import React from "react";
 import TarjetaComponent from "./TarjetaComponent";
 import FormularioComponent from "./FormularioComponent";
 
+{/* const usuario1 ={
+    nombre: 'Joseph',
+    apellido:'Joestar',
+    edad:'18'
+}
+
+const usuario2 ={
+    nombre: 'Josuske',
+    apellido:'Higashikata',
+    edad:'16'
+}*/}
+
+
+const usuarios =[
+    {
+        key:0,
+        nombre: 'Joseph',
+        apellido:'Joestar',
+        edad:'18'
+    },
+    {
+        key:1,
+        nombre: 'Josuske',
+        apellido:'Higashikata',
+        edad:'16'
+    },
+    {
+        key:2,
+        nombre: 'Jonathan',
+        apellido:'Higashikata',
+        edad:'16'
+    }
+]
+
 
 const UsuarioComponent = () => {
     return(
@@ -9,7 +43,9 @@ const UsuarioComponent = () => {
     <div className="row">
       <div className="col-8 ">
         <h1>Lista</h1>
-        <TarjetaComponent/>
+        {
+            usuarios.map(u => <TarjetaComponent key={u.key} usuario={u}/>)
+        }
         
       </div>
       <div className="col">
